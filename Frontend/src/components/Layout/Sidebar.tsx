fc59b7044318df6706252456eeb91cc99e8b086a
김지원 <wldnjs35242@naver.com>
2024-04-15 08:45:23 +0900

    :art: Feat: 사이드바 gsap 애니메이션 적용
    Former-commit-id: f23c1fe0365b72f1e0df7e43cc61b89608c056e7

--- Frontend/src/components/Layout/Sidebar.tsx
+++ Frontend/src/components/Layout/Sidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9681a2c537ddae4d295c7ac570a9319abe0ef0dfee905dc8024ef835007df93
-size 3288
+oid sha256:8a8a6ecfa184661a9dd3d890590a6c000039dc42c123797e01c17ce03d215b98
+size 4220
