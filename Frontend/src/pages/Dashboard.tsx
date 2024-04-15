950e043fbc1304602d8778fd0976ac6c2e741fcf
김지원 <wldnjs35242@naver.com>
2024-04-15 16:40:21 +0900

    :art: Feat: 사이드바 gsap 애니메이션 적용

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab5f55872c7612076ac93c2c5c3e702ef9777ca36dcfabd2fc586ea9c50a61e7
-size 854
+oid sha256:00b17c2a86769afc171a92579ad3de3a62a80b01096fd3858ee6714f9251548e
+size 891
