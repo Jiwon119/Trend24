018b46c2f06e4c3f4d643e5f202e0405be3ceb19
김태훈 <kth32105@naver.com>
2024-04-16 16:08:12 +0900

    :art: Feat : 로그인 페이지 레이아웃
    Former-commit-id: 5a2f6934cf5596b52aac0151071e7c65192f92ea

---
+++ Frontend/src/pages/Practice.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfa5e81d2a97f892d8f8ee15befcd6bb0826d4cb844472df603c6a08e4e2b66d
+size 3696
--- Frontend/src/pages/page2.tsx
+++ Frontend/src/pages/page2.tsx
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:13de59e3842952fd3c6f2e324159d1e8aa3780a66e4fcbe51db95769c97533f4
-size 69
+function Page2() {
+  return <h2>Page2</h2>;
+}
+
+export default Page2;
