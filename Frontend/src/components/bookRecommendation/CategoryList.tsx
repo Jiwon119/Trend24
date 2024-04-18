1a2cbdf45148947795ee6b75e085293cb9b10137
김지원 <wldnjs35242@naver.com>
2024-04-19 08:44:20 +0900

    :art: Feat: 사이드바 로고 추가, 색상 변경
    Former-commit-id: 66944815bd00c27a96077b9330ae76991d69bbd6

--- Frontend/src/components/bookRecommendation/CategoryList.tsx
+++ Frontend/src/components/bookRecommendation/CategoryList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6fcf8d1efeaaa5085e2772a67520248d60069e247fe26a9c273f1405292e3c
-size 1174
+oid sha256:c5d9a262179f64b305de0ed1b8d555f974c7735180efd31723774e7f2b1394c5
+size 1807
