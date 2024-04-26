92d652ef36f5cf2fba4a33644a1b97f15ba6a38f
김태훈 <kth32105@naver.com>
2024-04-26 16:56:17 +0900

    :art: Feat:드래그 가능 페이지
    Former-commit-id: 479fbf8d3fa152514decdaf1ae47f0e9db812c1c

--- Frontend/src/pages/UserCustomizePage.tsx
+++ Frontend/src/pages/UserCustomizePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cef1a1c2444249358b1b563f02ed9ff45ca9336a9edcdf45ec48373ccdaa1c7
-size 3148
+oid sha256:7076aa7a617af87f0fa489cca520443a43922ca5f82376ca93b95c03517f054b
+size 4390
