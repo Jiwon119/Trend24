e30ee3d3b70b6e6564f73977d336bc0bb69dfd39
김태훈 <kth32105@naver.com>
2024-04-26 16:56:17 +0900

    :art: Feat:드래그 가능 페이지
    Former-commit-id: da1f0e04388858df794459f612c954d0785b73c1

---
+++ Frontend/src/store/slices/customizeSlice.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c0461b6c94af46682cc06718336e43e48dc30ed5bfa729e5ba32689a2849043d
+size 787
--- Frontend/src/store/store.tsx
+++ Frontend/src/store/store.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b060e5663b1fa3b2d7383df78c2ed35c3e8a0847894ed0a4c1ca944264d03b9
-size 370
+oid sha256:d01b9f76eee75d5c3981bc58eceb3dc9530d67c9f20fdc3b564513f39d7e0617
+size 459
