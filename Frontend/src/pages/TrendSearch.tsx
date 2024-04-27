522b253b5c20f42a7dbc1e694b388e4430754411
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-04-28 02:09:19 +0900

    🎨 Feat: 라우터 수정, 사이드바 수정
    Former-commit-id: f5b2932a4a8f95ca8f8ea2f827ccca6199d56eaa

--- Frontend/src/pages/TrendSearch.tsx
+++ Frontend/src/pages/TrendSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a579280579adcfaa9de2e5354006de017718c7cc7d3cb5462aece922b8b0ac6
+oid sha256:dd6d182c6398ea2b247106939c91bdd319f4e8f60511aba7e1b46106e9357c30
 size 2335
