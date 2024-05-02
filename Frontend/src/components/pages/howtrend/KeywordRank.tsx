28ed877b7d71c0a58dcf81a72b00b5edd0fcda63
김지원 <wldnjs35242@naver.com>
2024-05-02 10:21:48 +0900

    🎨 Style: hotTrend 구조 변경
    Former-commit-id: 6f57cce632562a451e6081173fd3e7f28e7cd4e8

--- Frontend/src/components/pages/howtrend/KeywordRank.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a921c3d58c6845c494af0b7e4260127efa227ec282f72dbdc3bea01fb7e2a553
-size 1455
--- Frontend/src/pages/HowTrend.tsx
+++ Frontend/src/pages/HowTrend.tsx
@@ -1,22 +1,3 @@
-import styled from "styled-components";
-import KeywordRank from "../components/pages/howtrend/KeywordRank";
-import Table from "../components/pages/howtrend/Table";
-
-const HowTrend = () => {
-  return (
-    <HowTrendContainer>
-      <h2>인기 트렌드</h2>
-      <KeywordRank />
-    </HowTrendContainer>
-  );
-};
-
-const HowTrendContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: 100%;
-  border: 1px solid black;
-`;
-
-export default HowTrend;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a2e0f6a3fc6c3a34a985533717bad08c1d55796f3b430a73fa1d6fb2d095db2
+size 1861
