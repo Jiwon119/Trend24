d87a7b9fab6f61787d17f97ee926fb6ca6a8eacf
김지원 <wldnjs35242@naver.com>
2024-05-02 18:06:31 +0900

    🎨 Style: 디자인 수정
    Former-commit-id: a0fc0b5027995eaecc949cd56e5a4ccefa162bfe

--- Frontend/src/components/pages/howtrend/KeywordSource.tsx
+++ Frontend/src/components/pages/howtrend/KeywordSource.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cff9d86597c37797ddca9e81f8175c4d3fb9abfd19d938d6e4ac85293f51da4a
-size 290
+oid sha256:4d5d9df62166828ab0ced63ab0fe94f95a27c9c33b6c0a113b904907e4f4aae5
+size 1440
