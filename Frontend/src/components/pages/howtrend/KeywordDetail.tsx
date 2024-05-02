38046620e13dc09b76e3aa8283f80a7e65838d39
김지원 <wldnjs35242@naver.com>
2024-05-02 10:21:48 +0900

    🎨 Style: hotTrend 구조 변경
    Former-commit-id: 6ec67d86b344d0aed91a3adde5b956d28070aed3

--- Frontend/src/components/pages/howtrend/KeywordDetail.tsx
+++ Frontend/src/components/pages/howtrend/KeywordDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:843d90070b2cffd6137ac9cb0356f6eeae8f07dbd97b5f74e020d7dd49e88b38
-size 1302
+oid sha256:2dc6f053ed725f842a4f1e61d3214d4e8521b4dd5fe856cde7d8995282a5473d
+size 1364
--- Frontend/src/pages/HowTrend.tsx
+++ Frontend/src/pages/HowTrend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec36c57968f3ece888673f01cafc9b0da96bd1dfcfa9eade6fe1e413879496ec
-size 481
+oid sha256:8a2e0f6a3fc6c3a34a985533717bad08c1d55796f3b430a73fa1d6fb2d095db2
+size 1861
