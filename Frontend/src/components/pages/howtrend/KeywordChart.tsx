c687e932ccca86e5743f804a9be18e7ced790f4e
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-03 00:20:08 +0900

    :sparkles: Feat: hotTrend 페이지 키워드 컴포넌트 진행중
    Former-commit-id: 2aa3a32775f02f5b4bf35f3a4806d638b129b779

--- Frontend/src/components/pages/howtrend/KeywordChart.tsx
+++ Frontend/src/components/pages/howtrend/KeywordChart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9e3b2c7a052ce562c7b30b8ceac585fa0c7b2aa1f91b7563f82149ed2e27916
-size 718
+oid sha256:082b27f3042a83d34a873c1e408a688be3bda28682d595d15da3b8fc76b67107
+size 3190
