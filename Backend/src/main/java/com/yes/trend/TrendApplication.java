86e348fdab78649e30c8384cd767689df5a6ccdf
이정은 <silver36c@gmail.com>
2024-05-03 13:37:32 +0900

    ✨ Feat: 스프링부트 실행 환경 세팅
    Former-commit-id: d1171d9b2c8aee527a026b6c39bc8a91f556d1bd

--- Backend/src/main/java/com/yes/trend/TrendApplication.java
+++ Backend/src/main/java/com/yes/trend/TrendApplication.java
@@ -1,13 +1,3 @@
-package com.yes.trend;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TrendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TrendApplication.class, args);
-	}
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f45538f83b4e1f06d31e76b37202871b43c2f8ae3cecea93ba0e6053ddd0729
+size 627
