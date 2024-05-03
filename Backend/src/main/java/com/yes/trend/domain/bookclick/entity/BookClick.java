d40c1904ad623b597b5a46630000515dbc58cbd4
이정은 <silver36c@gmail.com>
2024-05-03 09:50:06 +0900

    ✨ Feat: 책 클릭 관계 수정

--- Backend/src/main/java/com/yes/trend/domain/bookclick/entity/BookClick.java
+++ Backend/src/main/java/com/yes/trend/domain/bookclick/entity/BookClick.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e30328dd2b2c934dac1b6fed2e35c1189b6500ea95a6324ebc6548c1a4a592cb
-size 435
+oid sha256:ebd997b246db3b8db4876ad39fa55810e1df66ba69ae1eedb69d06de36b0bb75
+size 506
