c3390d0090d839cd947b2877aafaffd390fe27e1
김태훈 <kth32105@naver.com>
2024-05-03 09:08:55 +0900

    :sparkles: Feat:페이지 제목 수정
    Former-commit-id: 5c3361ca4bfe1fdc23d06bcc69c16d12a00c0c03

--- Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
+++ Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade51cf720b92f79dd0d6748a9e9acd9a4e40f906be040c8130c26974112483f
-size 4604
+oid sha256:70c71fe3def04db20060c056184def3ebc1b38a74e10886c0333f149231417cc
+size 5635
