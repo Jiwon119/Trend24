fbd77a1d7575a9debc6531b5d88cdbab2186f18e
woori <woori50300@gmail.com>
2024-05-05 21:18:29 +0900

    ✨ Feat: Youtube자막 및 유지보수
    Former-commit-id: 114ff084f851f1b8f10adf9c673e8009b3e2be6b

--- data/youtube/api.py
+++ data/youtube/api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a685c9a0de60c2816c608a7863b9b2616d3ccbdfb150b37705f7459754e121a
-size 5222
+oid sha256:2efb12cc69aff4b0b376eb024e8b82e8fb4073bdc616cc9a1da33ca9372ab468
+size 5343
