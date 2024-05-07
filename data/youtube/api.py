ac3df44b9ebde42e5bbe038112002b25c5b7fab3
전우리 <woori50300@gmail.com>
2024-05-07 16:11:20 +0900

    🔧 Chore: Youtube API 수정
    Former-commit-id: cb6c048a6c9f1d5330e31202ffeccd0f2ed861e8

--- data/youtube/api.py
+++ data/youtube/api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2efb12cc69aff4b0b376eb024e8b82e8fb4073bdc616cc9a1da33ca9372ab468
-size 5343
+oid sha256:03c8a1fb6ce2805d2e459edca7dcf5da528c6b99db6106ca3580a44455bf1c5a
+size 5727
