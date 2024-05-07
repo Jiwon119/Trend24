e70e86deab9ec60c7ffb3148ea275ed58df46665
이정은 <silver36c@gmail.com>
2024-05-07 17:58:45 +0900

    ✨ Feat: Pageable 테스트
    Former-commit-id: 66168c49ef3e3d03bff134ed4f81d4b60e869c2b

--- Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
+++ Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380bdd7c3b9258ffa704626f0d721a7b2f8fa79374842b00d7eedeb2f9661ddb
-size 230
+oid sha256:844e15522adf7471f749d6220fcc2a37799f6fe02f40b6da541715a2992097c8
+size 364
