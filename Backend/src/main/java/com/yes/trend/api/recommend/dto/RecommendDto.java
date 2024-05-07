4ff6d62e40204e8273a7a7d9110ff19ebe9aeec8
이정은 <silver36c@gmail.com>
2024-05-08 08:18:06 +0900

    ✨ Feat: 책 추천 리스트 결과 DTO
    Former-commit-id: a43c51f12a4afa23cb729a6afb53fb40ef334a47

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,4 +1,3 @@
-package com.yes.trend.api.recommend.dto;
-
-public class RecommendDto {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5abe57fe2d15c1a1a40e83dc9d18320aece4dfb471c783a32e87909d86d89d34
+size 704
