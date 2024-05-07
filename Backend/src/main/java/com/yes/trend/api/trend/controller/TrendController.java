9bf438cc91e1011854866ae8615d51e0a7b0f0d7
이정은 <silver36c@gmail.com>
2024-05-07 14:44:15 +0900

    ✨ Feat: 1주일간의 일별 랭킹 상위 20개의 인기 트렌드 가져오기
    Former-commit-id: 14747658dc8975fe00f0a17824dc88b0657c38c9

--- Backend/src/main/java/com/yes/trend/api/trend/controller/TrendController.java
+++ Backend/src/main/java/com/yes/trend/api/trend/controller/TrendController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3df86d8f20dff2d53245e25ea85be6dea0b7dfa62880497a93f86dcd5df2207a
-size 353
+oid sha256:6fb836d9ad01e24d77c2633cd56ab4a75fa82a5cb0d411a50e8ecec5f0c7a218
+size 884
