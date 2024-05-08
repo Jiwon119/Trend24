a70819cab619c9dcc2bd8f114d88d08ea665b9f5
이정은 <silver36c@gmail.com>
2024-05-08 18:14:37 +0900

    ✨ Feat: 책 리스트 가져오기 (작업중)
    Former-commit-id: 539a82a2f1413be7d0204a1531daf281149c8097

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d28c0d8f3f07b33eea5ab41c807754ec961ad68173c9bf13ba5060e3682f1cd8
-size 736
+oid sha256:88422396ff09e7d0eab86767a0d33f7a45895bef3cf7cce91e9020913f93d752
+size 780
