4346a45a391524278a4d0a3d8359308520b38f29
이정은 <silver36c@gmail.com>
2024-05-09 01:10:23 +0900

    ✨ Feat: 키워드 없이 카테고리 목록만 가져오기
    Former-commit-id: 9de0b8dbdc81dea4a6d478c55bbdc02148bba8c9

--- Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82daac3f3b2dc3a14257ea2a1b6497ca4ab1a22b5c98369365315bd1e471c927
-size 1934
+oid sha256:17a51c4072b4a1c2e72b38df5717502ab81560f493e7b8a3f68e6dfe646a5025
+size 2386
