1dcb0851b14c01c64d38a4be029658cac2a972c9
이정은 <silver36c@gmail.com>
2024-05-09 00:47:18 +0900

    🔧 Chore: 스웨거 주석 추가
    Former-commit-id: dfe027de7f61537aa42148d9ec2630f8e940d682

--- Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3d79e62bb3316f9ba1137a869fcbf4b14c7733dd10ce0a7daf8b8a5662a2718
-size 1753
+oid sha256:82daac3f3b2dc3a14257ea2a1b6497ca4ab1a22b5c98369365315bd1e471c927
+size 1934
