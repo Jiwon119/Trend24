3ead57fa3cf0082566a65b1ecbde0007cdc1dadb
김태훈 <kth32105@naver.com>
2024-05-08 15:35:54 +0900

    :spakles: Feat:타입 에러 수정, 유저 페이지 가로 스크롤 추가
    Former-commit-id: 0fddb87b4a9ea9981121c0a2afae387b48bdb9cb

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a92db3e9e8dd82aa0131fa0be3ec2813a6f637c887cbc511ca3e1c73e8b0e074
-size 1705
+oid sha256:8d01a8578e52decf7f888465999f21cf8e2b8df263f0dd494702654b06f0da50
+size 1909
