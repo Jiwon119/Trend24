2e1e131ebb2ee2df66ebaf68a2f839692da2530a
이정은 <silver36c@gmail.com>
2024-05-09 02:14:10 +0900

    🎨 Style: reformat code
    Former-commit-id: 149d6c5d26e64c9bd7f26314f0ff4fdaa4eb1604

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/TrendCategoryWithKeywordDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/TrendCategoryWithKeywordDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7713d4928d9c3dfde8db6d4de6ae60ebe02c7e4b603bb988bd41c37aff852c
-size 700
+oid sha256:353aa7873af54d6666574f8e1c250dad7bfa9ffa4fe03e69a36ab85405ee4a1a
+size 667
