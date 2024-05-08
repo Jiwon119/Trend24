701225df305aae0c027a336e6512072721d33f19
이정은 <silver36c@gmail.com>
2024-05-09 02:15:39 +0900

    🔧 Chore: 스웨거 주석 적용
    Former-commit-id: 5ea5d9e9d8710732299c685aeb68abdc250a19d9

--- Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/controller/RecommendController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17a51c4072b4a1c2e72b38df5717502ab81560f493e7b8a3f68e6dfe646a5025
-size 2386
+oid sha256:a5fe92e45e0201ccb153058baf86e7ede2491d8c739f92f092ca78a71d7f59f5
+size 2503
