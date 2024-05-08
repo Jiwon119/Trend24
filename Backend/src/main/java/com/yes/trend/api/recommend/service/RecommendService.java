4d9676ce974bc68b0a77e468ae42312bca978fbc
이정은 <silver36c@gmail.com>
2024-05-08 20:49:11 +0900

    ✨ Feat: Response 형식 맞추기
    Former-commit-id: 79c3ba76b9affa64f68643463696bae2e7906d4c

--- Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/service/RecommendService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06ada171937c174fd1136396aa27ad86e293afed62d7ec682b0fe7c24d1cc970
-size 2153
+oid sha256:4e747f656238b03fbe64928e28bebc250a0481c57e46a47c8d4e38e2ab8c6df8
+size 2249
