945057d25c35a9a61f9baadba8000f881e9d2e5f
이정은 <silver36c@gmail.com>
2024-05-08 16:24:32 +0900

    🐛 Fix: 키워드가 있는 책 Dto 상속 받는 클래스 변경
    Former-commit-id: 04061a347aeb29533003fee4c66415314e7ea48c

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5abe57fe2d15c1a1a40e83dc9d18320aece4dfb471c783a32e87909d86d89d34
-size 704
+oid sha256:d28c0d8f3f07b33eea5ab41c807754ec961ad68173c9bf13ba5060e3682f1cd8
+size 736
