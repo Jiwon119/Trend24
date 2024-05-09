79ad2f68c5c9635437436be47b8e82f1cc92938c
김지원 <wldnjs35242@naver.com>
2024-05-09 15:14:52 +0900

    🎨 Style: 전체적인 글씨 크기 변경
    Former-commit-id: 6334d2dd52acf733b6f2f0f0920d95e2f6f6ae93

---
+++ Frontend/src/apis/apiConfig.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f49f691b4b388f1c3e7e0674c2e9b90e3c2e47840e1827883c26654a07102b36
+size 548
--- Frontend/src/pages/HotTrend.tsx
+++ Frontend/src/pages/HotTrend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fec143fe642a3cee7fdcfceb05c02489406c72b3172ebb5181f8df0b42019f45
-size 2222
+oid sha256:db9a79e98f0381ac66b80c54957992bce8ab7b6e77e899923e0c23eb73e50af6
+size 2684
