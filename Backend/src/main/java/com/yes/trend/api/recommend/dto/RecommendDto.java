a0da8038c06cc9761f0c6f3b767bc00bd16e235d
이정은 <silver36c@gmail.com>
2024-05-10 01:54:01 +0900

    🐛 Fix: 키워드 리스트 초기화
    Former-commit-id: 4d412b117248e73d4e955dbd777999ca81353fd3

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd4a4d6170225e45df97b06021e763c8e535496fa5cf0b816819a48b42491854
-size 1712
+oid sha256:693266a3f58dfda61c7b7491ca6cc2ab259fd14607e87a891775bd2f2fdc3dbc
+size 1744
