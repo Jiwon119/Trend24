f6dc3ded078a3bd9bd479430f4b0170e825fcbca
이정은 <silver36c@gmail.com>
2024-05-10 01:53:33 +0900

    ✨ Feat: 도서 검색
    Former-commit-id: e53c17c91ad0f3d4ef0934eac7d41fe09c7f44ba

--- Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
+++ Backend/src/main/java/com/yes/trend/api/recommend/dto/RecommendDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99c53c80c96d9d6bc70b4a4ad99f3a0f0db7c206bf271b3c9d720cff329a0acd
-size 1667
+oid sha256:fd4a4d6170225e45df97b06021e763c8e535496fa5cf0b816819a48b42491854
+size 1712
