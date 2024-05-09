9e7970e5d79b96a8a51e05d383a7bf8ebfb2ee26
김태훈 <kth32105@naver.com>
2024-05-09 09:38:49 +0900

    :sparkles: Feat: 이벤트 페이지 수정중
    Former-commit-id: 4818da7c24c967467fa9917413bba603a3111575

--- Frontend/src/components/pages/generalrecommend/GeneralRecommendBook.tsx
+++ Frontend/src/components/pages/generalrecommend/GeneralRecommendBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b72a54792fcb2be6d5cf76aa3346b516f16100e7ee64a07815eff2b7548b1a09
-size 5161
+oid sha256:36633da5733ee1d6788b66831aa56632ed0efa367641bc2cb1401603d05043c9
+size 5813
