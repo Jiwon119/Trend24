a24544a1a1add26a271f8ecd48466ad0d5946ad0
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-10 01:46:14 +0900

    🎨 Style: 디자인 수정
    Former-commit-id: d38b97adb576bb0fff324731a013894c620e9ace

--- Frontend/src/pages/PersonalRecommend.tsx
+++ Frontend/src/pages/PersonalRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fc6a47bccc0c10ab91d2e7e7a31f6a72bb53621ec909c54627a7041e703d0c6
-size 3289
+oid sha256:1e1e02c39021fa9f623db30afef3703b325d436f1f544faf9f3fe26c35b61f3b
+size 5227
