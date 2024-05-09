3b9e24ef8e6bac2dea2c7f61bd24b8646a5f0d51
이정은 <silver36c@gmail.com>
2024-05-09 23:00:42 +0900

    🔧 Chore: contents 미디엄 텍스트로 변경
    Former-commit-id: 591d1526a3841965b5bf9e52f21f7453d0d0e9a0

--- docker/trend-db/init.sql
+++ docker/trend-db/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01b27e91ccab77b1d37b6dc17373d893393fac629ea080c45de8e229e03a1316
-size 6818
+oid sha256:5dca032c2dc7d10a90e9f483e75f16176e637187bb950e0b5d3f671426b371c1
+size 6907
