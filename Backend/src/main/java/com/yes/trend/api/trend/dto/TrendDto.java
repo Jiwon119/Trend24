c3d2d522a3b2166ecc301dbf5adce2f845df50e6
이정은 <silver36c@gmail.com>
2024-05-10 00:34:19 +0900

    ✨ Feat: 키워드 참고 (출처) 데이터
    Former-commit-id: e993f2fdd02a3d12d548c084db840a96ebef546e

--- Backend/src/main/java/com/yes/trend/api/trend/dto/TrendDto.java
+++ Backend/src/main/java/com/yes/trend/api/trend/dto/TrendDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5782b9d72ddc7fcd080f79562d1080eae2be9919cf17129de6eb07103224794b
-size 758
+oid sha256:6865b76b4b52ee5780a86260e9f777215a28bba94a6d94763400bf6acd0812a2
+size 1066
