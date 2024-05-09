7fb5007baca6fd1ef29e65cae0b8e81b6ec50dc0
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-10 01:46:14 +0900

    🎨 Style: 디자인 수정
    Former-commit-id: bc22422538adf88489eea3d9244580ce6256ed6c

--- Frontend/src/components/pages/PersonalRecommend/PersonalSearchBox.tsx
+++ Frontend/src/components/pages/PersonalRecommend/PersonalSearchBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3212071b66e6f77911482d5dd661ccb77976d9a715ef4a88340e991eb039b7fa
-size 966
+oid sha256:befc09a876c7a15ef45e4ff819c0f52baff51bb7b0c9ff4679e3ee351603adf1
+size 1098
--- Frontend/src/pages/PersonalSearch.tsx
+++ Frontend/src/pages/PersonalSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da4bf069907c76731b2658c8df4cd13801aeb3e3bca1fe418e5657eea9a02f84
-size 1011
+oid sha256:dbf9783dd079c144a1b0047e56aa5b8edc7acc91fd196c88b978a8e08413c519
+size 1798
