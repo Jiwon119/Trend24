784aeb779f826780012c129fbb89f29c5285a307
김지원 <wldnjs35242@naver.com>
2024-05-09 10:33:33 +0900

    🎨 Style: 설문 질문 화면 디자인, 화면 전환 애니메이션 예시 추가, 우주 asset 추가
    Former-commit-id: d7857f6c40c56e63cf1df782512c1f1445e7729e

--- Frontend/src/pages/PersonalRecommend.tsx
+++ Frontend/src/pages/PersonalRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec6e77531c959e02352980196c9248d5400b5bee4d61bd4e6be4fc7aa4daa96d
-size 3618
+oid sha256:67f60abed6bd7b9ae7b8f2d5a347b3318fe9542bc818755382f55cacf89cb0b0
+size 3052
