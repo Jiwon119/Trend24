122f53a41934dc64fd57e9cdf2d666dc458adb3f
이정은 <silver36c@gmail.com>
2024-05-10 14:39:18 +0900

    🐛 Fix: 토큰 없어도 이용 가능하게..
    Former-commit-id: be132ba4ebdf6e73feee970d9180e12cb4962a82

--- Backend/src/main/java/com/yes/trend/api/auth/jwt/JwtFilter.java
+++ Backend/src/main/java/com/yes/trend/api/auth/jwt/JwtFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23f8323c503c153301fd005d53bee63a65eda7e702a482d5a21ac680d8f346fb
-size 3441
+oid sha256:e716ce6b2761352573fc768204744b939d10676c5fdee92c5847590add32a1c5
+size 3403
