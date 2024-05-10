3d7362753e2ca3784f73039851099b9b537d390e
김태훈 <kth32105@naver.com>
2024-05-10 16:39:53 +0900

    :art: Feat: 첫 로딩 시 로딩 중 추가
    Former-commit-id: 1f689bd25056634ad11cb036ca9297875c1e40ae

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:839e28a4b97609c6cea390bac9bdc9aece52ffa488f2864990669aadc21b37b0
-size 2350
+oid sha256:651cac90dfb689e0fb04b00260064bef13c10a6db75d21ddd75a72de19af74a1
+size 2815
