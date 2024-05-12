4d823f6b9df9c9e0c68c5262cfdca74e4d570aba
이정은 <silver36c@gmail.com>
2024-05-12 21:07:01 +0900

    ✨ Feat: mysql test connection
    Former-commit-id: 0015920638532c52887b57dce1874c114869d817

--- PythonAPI/pythonProject/api/mysql_insert.py
+++ PythonAPI/pythonProject/api/mysql_insert.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7840244d5931a12ca02cbef8be38b7020f77237e1e7bc60191c8692c6b075ec8
-size 8689
+oid sha256:6ff096ffd46aa13fe78b548fc23accfa339053fc2cedad193489ba9b83c36a62
+size 8977
