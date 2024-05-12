5f926f3da76001ea3249dd31293a805b80a9f850
이정은 <silver36c@gmail.com>
2024-05-13 02:29:57 +0900

    ✨ Feat: 컴포넌트 리스트 조회
    Former-commit-id: d6112eafcd0d4809d7b68f134f7ec686ceb563cc

--- Backend/src/main/java/com/yes/trend/api/custom/service/CustomService.java
+++ Backend/src/main/java/com/yes/trend/api/custom/service/CustomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3876aa9219c7b035873069f16d63217e3e1f3c120e0992e2878979023d880626
-size 1149
+oid sha256:6565cce7041fecd7b80bee977a973fd71f6ceeaed0ee6236c7af5bb8e16d045b
+size 1394
