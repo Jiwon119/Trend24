542f07eacca551cd29f3661fb46d08168be3011e
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-14 00:53:27 +0900

    :sparkles: Feat: 트랜드 검색 날짜 선택 기능 추가
    Former-commit-id: 2c58b4b554193658a84f9bc8f40c4d0ff718abae

--- Frontend/src/pages/TrendSearch.tsx
+++ Frontend/src/pages/TrendSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10c2e019d115d63175dadc22c11673008bdb7d7a9d0153ad8e4173467aee3663
-size 3944
+oid sha256:09010106b40e93db97c1f449776f1b400445226d71e60bc0d8ca9abc3aab3c1d
+size 4218
