e779a7213ff5bbc9759b45c593a7f2b462ddb6d4
이정은 <silver36c@gmail.com>
2024-05-13 15:04:44 +0900

    ✨ Feat: AU-01 로그인 회원정보 반환
    Former-commit-id: ba4025bc1e3c615b7b84e403d2fcb3b7892a07f1

--- Backend/src/main/java/com/yes/trend/api/auth/dto/AuthDto.java
+++ Backend/src/main/java/com/yes/trend/api/auth/dto/AuthDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1be61244974b0bc37f2bb1f2652c54e50a9f80e0f36f8d4f91e457a27b58826c
-size 913
+oid sha256:ccad2ddc2dc3f9457ce477feac5d8d3f3438dae43700d37afc84218d9e532fa1
+size 1391
