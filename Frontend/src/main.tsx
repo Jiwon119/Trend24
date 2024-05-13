6962f7c112a4b00f65d01c68ca1b8eb4302387ee
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-14 02:20:50 +0900

    :sparkles: Feat: 로딩중 페이지 디자인
    Former-commit-id: d2e8fa9851a0275fb1a92f0fe32bb52093aacbef

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdec98e0e87687952567a8817e7eebba684874fac390b000da6ce6b22189fbca
-size 438
+oid sha256:822a48035423ad2bcbe19126fe4c1f7c0442eb360c4f98f0891b2233ceffff9e
+size 1100
