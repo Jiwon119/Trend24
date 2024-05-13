e89e90a9ed285b0f712c97d2b148d43bec4fd503
이정은 <silver36c@gmail.com>
2024-05-13 15:53:08 +0900

    🔧 Chore: 파이썬 의존성 도커 컨테이너 실행문 삭제
    Former-commit-id: a8baddd3c2f172234de66fed4d4c6eb9247b7fe4

--- docker/trend-fastapi/build-requirements-image.sh
+++ docker/trend-fastapi/build-requirements-image.sh
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:182f7a52abd592bf99789e9f4738fa820c085fcdd4c05ba551079a5177d8e517
-size 633
+#!/bin/bash
+
+# 스크립트 실행 전에 현재 위치를 저장합니다.
+original_dir=$(pwd)
+
+# 스크립트 실행 위치를 Backend 디렉토리로 변경합니다.
+cd $(dirname "$0")/../../PythonAPI/pythonProject
+
+# .env 파일을 읽어 환경 변수로 설정합니다.
+source .env
+
+
+IMAGE_NAME=trend-fastapi-requirements
+FILE_NAME=Dockerfile-requirements
+
+
+# 현재 폴더의 Dockerfile을 사용하여 이미지를 빌드합니다.
+docker build -f $FILE_NAME -t $IMAGE_NAME .
+
+echo "스크립트 끝"
