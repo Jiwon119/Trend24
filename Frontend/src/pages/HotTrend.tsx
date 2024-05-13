ecb54bac38a55e98f8512e58bff09048aaeac227
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-13 23:13:21 +0900

    :sparkles: Feat: 키워드 추가 api, 트렌드 검색 api
    Former-commit-id: e388ffae64189ee764fd6931e94bafcb23cc9411

--- Frontend/src/pages/HotTrend.tsx
+++ Frontend/src/pages/HotTrend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca766d89c708ae5e5e9e8ac64142df5656d8b139aa8e5a2bb317d0b9943f9bbe
-size 3986
+oid sha256:22db414932c87f7af5ba8958bf80bfc8c8eadb247266dc87030f0f428a5e506e
+size 5251
--- Frontend/src/pages/TrendSearch.tsx
+++ Frontend/src/pages/TrendSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86ebda77a89ad099d603141504c5ee6f806bf2d3866d463446a4deba39b1fdaa
-size 3584
+oid sha256:10c2e019d115d63175dadc22c11673008bdb7d7a9d0153ad8e4173467aee3663
+size 3944
