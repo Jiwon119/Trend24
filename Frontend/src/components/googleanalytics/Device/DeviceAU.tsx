a748ad2a5428e5297ba3875c0850b4fddc3add32
김태훈 <kth32105@naver.com>
2024-05-14 02:04:10 +0900

    :sparkles: Feat: 보고서 레이아웃 수정, 이벤트 메인화면
    Former-commit-id: 3ba0690e0fded75226677f22aa53efa65d9b446b

--- Frontend/src/components/googleanalytics/Device/DeviceAU.tsx
+++ Frontend/src/components/googleanalytics/Device/DeviceAU.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bca39a41f1adb89e9e63faab1c7ddfcaba5f62e09f15e5dd267198169edcff5
-size 2678
+oid sha256:1cb08f3c07666230bee97374fc885ee95ef5cd509fe953e98f8c805dabbc7102
+size 2704
--- Frontend/src/components/pages/event/Main.tsx
+++ Frontend/src/components/pages/event/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf5abb6d20e72d5a39a3d4c10ccfa835c89672cd812fe4f5648344b41a36b09b
-size 7400
+oid sha256:288b6271a624ed456b67d748168a24515dad755b7512ead28813ae31f593380a
+size 12913
