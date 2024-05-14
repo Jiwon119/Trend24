f5d8f0c778a7751ca5ef71c59de96a5ebad2364a
minahpark42 <minahpark42@gmail.com>
2024-05-14 11:18:09 +0900

    ✨ Feat: 키워드 누적 클릭수 리스트
    Former-commit-id: 51eae3c2abebb07305cc749ffcf315a08eafd167

--- Backend/src/main/java/com/yes/trend/api/status/dto/StatusDto.java
+++ Backend/src/main/java/com/yes/trend/api/status/dto/StatusDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94acb5f33fd2206c2f0949863ca514c98b54817ee75477abfa92f142b1d8fb4f
-size 974
+oid sha256:6e864986fbbd8daa42bdcf18a2bbd0d6341db5a407501246bfe9c94f49e792d1
+size 1090
