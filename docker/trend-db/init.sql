137c5e6a2258b10440930b5863422db16a7fd5cc
전우리 <woori50300@gmail.com>
2024-05-14 09:42:10 +0900

    🔧 Chore: 재수정
    Former-commit-id: f0728bd84727a196a1e6002a0b6f9998f3e457bd

--- docker/trend-db/init.sql
+++ docker/trend-db/init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60f90a3d8013f5d1e377b98bc4b32a1f46bd67b179fef650a85896a946254b65
-size 8422
+oid sha256:93f20edb4f6e5be173b068750e4f421785e5cf7b7ecc26e4a68f5227b80a9f81
+size 8566
