6f73858bd320b81101570af874bc4dac524724ef
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 15:42:59 +0900

    :sparkles: Feat: 사용자 페이지 QB-04, QB-01
    Former-commit-id: c4450e7ab04b7b43fbf6d6d1b98ea10d5103d975

--- Frontend/src/components/pages/PersonalRecommend/PersonalSearchList.tsx
+++ Frontend/src/components/pages/PersonalRecommend/PersonalSearchList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a3cfdccc03863d2526a7ff057fed00d415871eeea394fca5f0fbd2d48b26024
-size 985
+oid sha256:ffc7e8aa7547a2cadd027f9e1af0ec72c26e640a8b17f0f242d7b57c3eafdc2b
+size 1262
