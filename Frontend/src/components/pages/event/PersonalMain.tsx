da5ef066607ee49e930181761ec2c6c0995ac2a5
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 21:31:07 +0900

    🎨 Style: 사용자 메인페이지 로켓 애니메이션
    Former-commit-id: ee390ebbffb32935312d495ac54c58d96b1a37ba

--- Frontend/src/components/pages/event/PersonalMain.tsx
+++ Frontend/src/components/pages/event/PersonalMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54e56b501a5d8b3ebbb89a9287b3ea20cd87ce2396fa4f1f7caa1038bcc858d6
-size 3329
+oid sha256:3046c9ce5cb4c18d98d2a8b18e6d86525ddde785f7754d5762c525a6485196ad
+size 4180
