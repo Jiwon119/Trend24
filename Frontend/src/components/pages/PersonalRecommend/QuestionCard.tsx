f6e80c4ca911eea67c09bcb8c39eccc7bfbfed53
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 15:42:59 +0900

    :sparkles: Feat: 사용자 페이지 QB-04, QB-01
    Former-commit-id: 425e92d3caf8620976bb424225f62562286c5726

--- Frontend/src/components/pages/PersonalRecommend/QuestionCard.tsx
+++ Frontend/src/components/pages/PersonalRecommend/QuestionCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d84da550dd23d4a5cbc002b3bb0fbd6cb693a459efc1cbed2c28cc3a121de52
-size 1492
+oid sha256:7870d3badc90c65425b7fa1aa8c686a8a7955c11c9e8e16b45f668fdedbe7dd1
+size 1543
