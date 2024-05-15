73dad23319d583ad987e93729ed33291e604d0e0
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 13:06:30 +0900

    🐛 Fix: response 형식 변경
    Former-commit-id: 06e70e750e34fee64e9abfa4a6c55b2735c586b9

--- Backend/src/main/java/com/yes/trend/api/anonymousquestion/controller/AnonymousQuestionController.java
+++ Backend/src/main/java/com/yes/trend/api/anonymousquestion/controller/AnonymousQuestionController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31970abad7f5167783d4f066ba594d6e0d32fbaab31d0d14cf4234a7d51d7f78
-size 1981
+oid sha256:738eef10fbdc2d86a89f9fad551fa2113ea5fe2b42045dfcc7e87d2c051629bf
+size 2049
