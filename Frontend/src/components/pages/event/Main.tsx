73af7727cb396e303fd139542a2692cb99ea685e
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-15 18:55:44 +0900

    🎨 Style: 사용자 메인 디자인
    Former-commit-id: 8e401a63720a2d9f4e4211a0a1330a0d200b05e2

--- Frontend/src/components/pages/event/Main.tsx
+++ Frontend/src/components/pages/event/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8468a6342cf8bbb4ade1ae2fa5e6aef73351d3065f543e140c7f5111369ec03b
-size 12908
+oid sha256:c2e5d35ac66697ea75655659649d1d236fa0ce71aa65deacacda2a0f545cc8af
+size 13020
