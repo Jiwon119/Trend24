0b911f7a2e046a47d65759911433cfdb4d9a8d85
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-17 03:25:04 +0900

    :sparkles: Feat: 사용자 페이지 개인 설문 api 연동 완료, 디자인 완료
    Former-commit-id: d3a0cc908ef240c3502ee7deede0fc96fd34103c

--- Frontend/src/pages/BookSearch.tsx
+++ Frontend/src/pages/BookSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab624f9d2313c2a4b35fc01f04fb8872e424e86daed1a44431401bc524c75874
-size 3344
+oid sha256:df015c1185e21cff1ee0dc1a88acc6b9a7da53747595b1373942475d0b2d30c1
+size 3384
