0d3882da179b121cd0981dc89f0942b65f1d82ca
김태훈 <kth32105@naver.com>
2024-05-16 10:50:04 +0900

    :fix: Feat: 에러 수정
    Former-commit-id: 3c46a18543a45d00aa08080275aa8bedbf64278b

--- Frontend/src/apis/searchApi.tsx
+++ Frontend/src/apis/searchApi.tsx
@@ -1,27 +1,3 @@
-import { api } from "./apiConfig";
-
-export const getSearchBook = async ({
-  title,
-  category,
-  page,
-  size,
-}: {
-  title: string;
-  category: string;
-  page: number;
-  size: number;
-}) => {
-  try {
-    let url = `/search/?page=${page}&size=${size}`;
-    if (title !== "") {
-      url += `&title=${title}`;
-    }
-    if (category !== "") {
-      url += `&category=${category}`;
-    }
-    const response = await api.get(url);
-    return response.data.result;
-  } catch (error) {
-    console.log(error);
-  }
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:b50e08a3510ddb377da8d3c39613ca7c6e31b2b68580285646176674f36e1a20
+size 511
--- Frontend/src/components/googleanalytics/UserActivityDaily.tsx
+++ Frontend/src/components/googleanalytics/UserActivityDaily.tsx
@@ -1,26 +1,3 @@
-import styled from "styled-components";
-
-const UserActivityDaily = () => {
-  return (
-    <Container>
-      <Title>일별 사용자 활동</Title>
-    </Container>
-  );
-};
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-`;
-
-const Title = styled.div`
-  margin-top: 20px;
-  font-size: 24px;
-  font-weight: bold;
-`;
-
-export default UserActivityDaily;
+version https://git-lfs.github.com/spec/v1
+oid sha256:52140a6a31da0efb76f3f2c0a5d07295e0503646f1d8a96764647d7cd40ef17c
+size 455
