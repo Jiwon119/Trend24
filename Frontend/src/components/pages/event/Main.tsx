302d2de0d748913427c8bb027128f7c86b325775
김지원 <wldnjs35242@naver.com>
2024-05-16 14:18:26 +0900

    🎨 Style: 메인페이지 사이드바 수정
    Former-commit-id: 8a24ef5ad66f9aa2a3c4b22bbdee4c198a770799

--- Frontend/src/components/pages/event/Main.tsx
+++ Frontend/src/components/pages/event/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d97222d21e274c665b6249e3bb3922f3c357ea63b4ad39711de649c692bc0e3
-size 13509
+oid sha256:b38fd640c1ebe5a4b9b786f45c910fb20328fc5c2449c87cb05e026ea15e7988
+size 13458
--- Frontend/src/components/pages/event/PersonalMain.tsx
+++ Frontend/src/components/pages/event/PersonalMain.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3046c9ce5cb4c18d98d2a8b18e6d86525ddde785f7754d5762c525a6485196ad
-size 4180
+oid sha256:3b475915014a34041eb3883cc59420abaf376474fa0955c4519fdb842c9869de
+size 4281
