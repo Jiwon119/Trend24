102b8992f441e037e740390e0279bcb26edff4db
전우리 <woori50300@gmail.com>
2024-05-16 17:18:43 +0900

    🐛 Fix: QB-4 책 목록 리스트 코드 재수정
    Former-commit-id: f5273f112721a73b315412791650f059aef03519

--- Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
+++ Backend/src/main/java/com/yes/trend/domain/book/repository/BookRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8b85945f55d02dc7cab1bd0b5475f7685a40eda834925b3dca5eb8c58bc75f1
-size 1067
+oid sha256:36ab6e1cab74c0b847f181be65962dd339b460dbaf3affa1e42fac755cdd6c5f
+size 930
