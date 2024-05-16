52bc98b644f5f203555704ceb04accca4e12e131
김지원 <wldnjs35242@naver.com>
2024-05-16 17:43:52 +0900

    🎨 Style: 사이드바 디자인
    Former-commit-id: 405dd5ef33a71a712b012abde61ae48a72ffd949

--- Frontend/src/components/pages/event/Main.tsx
+++ Frontend/src/components/pages/event/Main.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27edb567ed6b08685fbadcb5aea4a9e2eefd9bf5468db532542cc2a85720b304
-size 13465
+oid sha256:8ebc20489c36e826c09990c1a17b09adf782200ac86420d64b7cc200c21e2aa9
+size 13846
