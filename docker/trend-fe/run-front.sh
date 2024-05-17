0ec62a0916cd3381c79dcc66d3062180dbbff007
이정은 <silver36c@gmail.com>
2024-05-17 14:04:06 +0900

    🔧 chore: 포트 변수에 저장
    Former-commit-id: 44123e77f4889d5a4153d20c4f556984d571288e

--- docker/trend-fe/run-front.sh
+++ docker/trend-fe/run-front.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bc9a059b5bf567b58b508df1919ad9ba886b6922f805d6a00c637aa7d3ea699
-size 1372
+oid sha256:00ec726381ca3776bd162f8fef46a81ea980200e124e518ce1b3fcf554311f68
+size 1395
