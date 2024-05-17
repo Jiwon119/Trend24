7c5688cd46b2e366619993fccc0785239f0bae05
minahpark42 <minahpark42@gmail.com>
2024-05-17 14:58:44 +0900

    ✨ Feat: 책과 키워드 클릭수 업데이트 기능
    Former-commit-id: 4ab239322ca3a0009d094856793c247a79835287

--- Backend/src/main/java/com/yes/trend/domain/recommendkeyword/repository/RecommendKeywordRepository.java
+++ Backend/src/main/java/com/yes/trend/domain/recommendkeyword/repository/RecommendKeywordRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfe311686eac9515e5dcd1f66c80c9acdf817813804c6b8400b247b6b7ad650e
-size 1720
+oid sha256:e3d8ce6393eef0e3e5be8f6759b14e8e144bdc529981da20030e028d9dcea5f4
+size 1863
