afb55f43fa2a2d479b0e94b1ff6313b7a6ed8468
이정은 <silver36c@gmail.com>
2024-05-18 02:59:13 +0900

    🐛 Fix: 기간 1주일로 변경
    Former-commit-id: 0577724a12fcc197dd3551eb8269de4a588e3fb5

--- Backend/src/main/java/com/yes/trend/api/anonymous/service/AnonymousService.java
+++ Backend/src/main/java/com/yes/trend/api/anonymous/service/AnonymousService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:153ab562deae4624ff91fa1b598b930e7a5199406daf36c44579e0a81ff90a87
-size 4805
+oid sha256:ecd8b0104a28a17f264028479d4026e7399c1a3b05e6c0b3f7a815d3e15e78fb
+size 4761
