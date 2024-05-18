ea6cce5415ac709ccff0ca803c4863f4ca08cdca
이정은 <silver36c@gmail.com>
2024-05-19 02:11:20 +0900

    ✨ Feat: 도서 문장으로 검색
    Former-commit-id: 69ed2e0b497cb52063868ab0b438f980967ba68e

--- Backend/src/main/java/com/yes/trend/api/search/service/SearchService.java
+++ Backend/src/main/java/com/yes/trend/api/search/service/SearchService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fab28b55c9656e52b75a73d08f6482417933f8680c9940a9261e016de14abed0
-size 1655
+oid sha256:f58e3ae036f6ecbb6d8cfbe9db2c1271989e5f5c047027f217eee40fcafd5ba5
+size 3113
