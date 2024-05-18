9436dcf0ef396200b18477ca8c52bde7785427e7
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 01:40:04 +0900

    Update SecurityConfig.java
    Former-commit-id: a655f9f74f5dc51e806214e665dc98023a98cab2

--- Backend/src/main/java/com/yes/trend/common/config/SecurityConfig.java
+++ Backend/src/main/java/com/yes/trend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262125248c087c9eda269ddf200bfbc8700e43e4bde92e5cc1c53816dec26ab5
-size 4542
+oid sha256:ece5800a160bf1a2d6a048b57020a4de5cd0b0cb4451be6acdb5dfbdd126f8fe
+size 4536
