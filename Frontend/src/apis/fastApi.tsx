207ca46bab6aeec9ed737a50fed2d1fc7c82f81e
김지원 <wldnjs35242@naver.com>
2024-05-18 18:04:41 +0900

    🔧 Fix: fastApi URL 수정
    Former-commit-id: ef2d1d3545810ef9ce086246183fef6132b1ce4b

--- Frontend/src/apis/fastApi.tsx
+++ Frontend/src/apis/fastApi.tsx
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cf9b3028cb245a3f4e0e1a1ef994617d9e15ee5be8aa92df738544bcc4579bb9
-size 792
+import axios from "axios";
+
+const fastApi = axios.create({
+  baseURL: "http://trend24.live:8000/fastapi",
+  headers: { "Content-Type": "application/json" },
+});
+
+export const getBookLive = async (text:string) => {
+  console.log(text);
+  
+  try{
+    
+    const res = await fastApi.get(`/book/live?search_sentence=${text}`);
+    console.log(res);
+    
+    return res.data.result;
+  }catch (error){
+    console.log(error);
+  }
+};
