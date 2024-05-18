f1087cc11bbc269d0f591c389b81cf00ccbcf958
김태훈 <kth32105@naver.com>
2024-05-18 11:27:15 +0900

    :sparkles: Feat: 수정
    Former-commit-id: fdcc23872c783776143da8e79f9ed1bbcf6c2d28

--- Frontend/src/components/pages/generalrecommend/BookScroll.tsx
+++ Frontend/src/components/pages/generalrecommend/BookScroll.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9583144caff8f185df63f79d942eaa1d830903347ac3c7b98dc214ab87d34f17
-size 6750
+oid sha256:49f0bf56c8c4f613a3d6b6c71af533e59511a44fcbd7419353d87b389fdefc3c
+size 7415
--- Frontend/src/components/pages/generalrecommend/GeneralRecommendBook.tsx
+++ Frontend/src/components/pages/generalrecommend/GeneralRecommendBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ee09e1216223984bd1ec841b722155ca098e5ca7d9eaf1d80649938ae8c7d25
-size 14149
+oid sha256:4662080da9a6e2588a6138bf99e9d36d64c82947b89d8b8bb13e8a3193bd0827
+size 14940
