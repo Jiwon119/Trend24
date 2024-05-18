a70e600a98d08f6722fc3cc062b2cac67699e434
김지원 <wldnjs35242@naver.com>
2024-05-18 18:04:41 +0900

    🔧 Fix: fastApi URL 수정
    Former-commit-id: a07663220a56c09499be43278bb43e648279410c

--- Frontend/src/apis/fastApi.tsx
+++ Frontend/src/apis/fastApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf9b3028cb245a3f4e0e1a1ef994617d9e15ee5be8aa92df738544bcc4579bb9
-size 792
+oid sha256:a10214d62ed15f2c9a5f4b0c6f6a16729cb1723103fa98fa37e38c2991e4620e
+size 426
