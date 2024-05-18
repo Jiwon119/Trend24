8c731e866ce432c038aac09eddb1c7d9f4e276a6
김지원 <wldnjs35242@naver.com>
2024-05-18 12:25:50 +0900

    🐛 Fix: CORS 설정 추가
    Former-commit-id: e186b4e4b6b9a78ad9b78296b5fe6cffe9211202

--- PythonAPI/pythonProject/main.py
+++ PythonAPI/pythonProject/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ee3f4c5d061f4b103024804b33bcfba9ef5172ba2c970fb575cf7150dc29fd
-size 3475
+oid sha256:6e5c49cad38b7a59c368c155bc40c35ae12045fee8988b3340ac580fead4a4af
+size 3718
