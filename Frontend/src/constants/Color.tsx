e8c0b886a5abe13111e9c29b0810313bfec2649a
김지원 <wldnjs35242@naver.com>
2024-05-19 01:04:54 +0900

    :sparkles: Feat: api
    Former-commit-id: 4e0f6c9c675319fd14d3c0035cb8ea8bde0d33d0

--- Frontend/src/constants/Color.tsx
+++ Frontend/src/constants/Color.tsx
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:df32edd2bc49feff713fbbeed8e54b068847e980bb634312fe2beb4229fe349a
-size 412
+// export const MainColor = "#11101d";
+// export const SubColor = "#EFF3FC";
+
+const Colors = {
+  main: "#5f996d",
+  sub1: "#77a081",
+  sub2: "#8da392",
+  sub3: "#9ba09c",
+  sub4: "#c2cec5",
+  containerBackground: "#ffffff",
+  screenBackground: "#edf0ec",
+  text: "#000000",
+  hover: "#C1E1D2",
+  // F5F5F5
+};
+const Shadow = `-3px -3px 7px #ffffff73, 3px 3px 5px rgba(94, 104, 121, .288)`;
+
+export default Colors;
