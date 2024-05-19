820ecd8b589f859023d3e37fae35cb34b9020fe2
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 18:46:46 +0900

    🎨 Style: 서랍 추가 폼 디자인, 유저 질문 책 표시 개수 설정
    Former-commit-id: b9512c213bf22a252aa01241da6697c4e1b0d673

--- Frontend/src/components/pages/PersonalRecommend/PersonalSearchBox.tsx
+++ Frontend/src/components/pages/PersonalRecommend/PersonalSearchBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e580351c27f2e93328ae97629d3c0fd89c2cec1ad5e0898484968b97bb5d37f4
-size 5068
+oid sha256:23f442ecad7095bce0cae66c0bc63877aa91bbc6d2fbc56a07564a0ba8947aef
+size 5081
