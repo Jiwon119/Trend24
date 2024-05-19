5479c2cc8824a2f59db386ff96e4314e029186b3
김태훈 <kth32105@naver.com>
2024-05-19 21:16:10 +0900

    :sparkles: Feat: 수정
    Former-commit-id: 4d64df5344be102e341b61258ef1f0b001901b8b

--- Frontend/src/components/pages/useractivity/user/BookClicks.tsx
+++ Frontend/src/components/pages/useractivity/user/BookClicks.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd3f0b924238ed5b0a35ba2b89b7b4f8cd8e50164de4b77a425cffa807f64131
-size 7551
+oid sha256:20e927609c2592b4b49370737478b79311f7450e4a50525e0cd44ebaf3df2133
+size 7553
--- Frontend/src/components/pages/useractivity/user/KeywordViews.tsx
+++ Frontend/src/components/pages/useractivity/user/KeywordViews.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5686618d36fa1becf95633f50bf39b717de1f07935925108ae33f851b100569f
-size 9609
+oid sha256:72ed37c2876ec91eddfbd8aa4b51b273ed8ce4f02180a7c7a9fcb6745464276f
+size 9614
