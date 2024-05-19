4a2a7151502e1c47dca9cedd4aef34f5710ed030
김태훈 <kth32105@naver.com>
2024-05-20 08:57:03 +0900

    :fix: Feat: 오류 수정
    Former-commit-id: ed3a511685aadbeb242be4e9f08d3f5163fd38da

--- Frontend/src/apis/customApi.tsx
+++ Frontend/src/apis/customApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba3859da59022bd6de4f7ca1b1744657a08870541d1bf063afb794b50eae44a4
-size 2068
+oid sha256:ea6bf7615f81ed4dc2c76ccbfa3d2aff7c844227ff1670fc59f6428a0823237a
+size 2139
--- Frontend/src/components/pages/useractivity/customize/RnDCustom.tsx
+++ Frontend/src/components/pages/useractivity/customize/RnDCustom.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9181538cd280df590790e9719d41d2b7fcf465a83481477dd5c886844f2054a7
+oid sha256:fba5bb8af472e6f8c136b1d5e2c1fbcdd1a536a35fe1edb69a07c43d98045cf1
 size 9895
