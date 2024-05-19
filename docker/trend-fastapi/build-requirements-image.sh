b029daf9880cd83ffb4ad786bba0577cc361ea0f
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 20:59:26 +0900

    🎨 Style: 디자인 수정
    Former-commit-id: 1260844554c6cbf84ff45680076ff47f52c646ad

--- docker/trend-fastapi/build-requirements-image.sh
+++ docker/trend-fastapi/build-requirements-image.sh
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:70da51d9043b2f3d2e11a05f14c85b274d6b01a25622332f88e0961b8b39b070
-size 510
+#!/bin/bash
+
+# 스크립트 실행 전에 현재 위치를 저장합니다.
+original_dir=$(pwd)
+
+# 스크립트 실행 위치를 Backend 디렉토리로 변경합니다.
+cd $(dirname "$0")/../../PythonAPI/pythonProject
+
+# .env 파일을 읽어 환경 변수로 설정합니다.
+source .env
+
+
+IMAGE_NAME=trend-fastapi-requirements
+FILE_NAME=Dockerfile-requirements
+
+
+# 현재 폴더의 Dockerfile을 사용하여 이미지를 빌드합니다.
+docker build -f $FILE_NAME -t $IMAGE_NAME .
+
+echo "스크립트 끝"
