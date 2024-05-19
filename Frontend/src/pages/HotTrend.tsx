7793fa04b8ac399189d75cb44aacb75b7928d23b
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 18:59:15 +0900

    🎨 Style: 인기트렌드 hover 효과
    Former-commit-id: 0482d0810a4a621103df1aabdfe1f781e3e87629

--- Frontend/src/pages/HotTrend.tsx
+++ Frontend/src/pages/HotTrend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb70ee1428236e6ac8bb8c304ea5cdeaf69022fc6ba71370d5a4808da0707dd8
-size 5336
+oid sha256:136f565af880925278ce6ea2e6dc303f011e7bb8b7bb6cb52e998965f91784bf
+size 5599
