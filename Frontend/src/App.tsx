e696c174e958b75cba2016e5e3d0fa1eccc51ce3
김태훈 <kth32105@naver.com>
2024-05-20 03:43:19 +0900

    :fix: Feat: Protected Routing
    Former-commit-id: 306c0ebea9e910b4e6d5935cce3c4dac5633c7a6

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:994f39fbb9506decc41a7d76597bcfd141d3149592a5856548effddca9cb889c
-size 3691
+oid sha256:08346a4b4b753b56f386cf777abe607e6aec603bf7ec41e10f41a647fb0f9aaa
+size 4347
