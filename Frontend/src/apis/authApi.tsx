ed65787038be0eeec48262d6e60c5fe2f99f941d
김태훈 <kth32105@naver.com>
2024-05-20 08:57:03 +0900

    :fix: Feat: 오류 수정
    Former-commit-id: 49da944d0ffcb73add6d0798abe0c60633cb3506

--- Frontend/src/apis/authApi.tsx
+++ Frontend/src/apis/authApi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ef41c7b961e3d2a5a767eb3e85e0eb8e5977a757495b2c662ac55106ec8b2e9
-size 540
+oid sha256:8e0c3d4b372efeeba836dbbe7819d851272385539c16934ca2adbd0122c44695
+size 542
--- Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
+++ Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d395c05669f662a1dd7bc4b8a8e544193b4a42a87ee6e0e00fb9df85472ecac
-size 5654
+oid sha256:3b3d07d649c19e0a9a96dd994f51c41e3036025a66069cf352311f5c6bee9ea0
+size 5655
