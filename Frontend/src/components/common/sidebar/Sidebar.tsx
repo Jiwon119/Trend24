b67097ae76602ac2baa7326c05774e9e4e0ed643
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 19:06:25 +0900

    :sparkles: Feat: 로그아웃
    Former-commit-id: f238e329d1b4d1907e2c6ea1df71dd04d889373a

--- Frontend/src/components/common/sidebar/Sidebar.tsx
+++ Frontend/src/components/common/sidebar/Sidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b8fb61410b265c47325ca62ad089bca96888a13e78d4c0105312fe404e8536f
-size 7046
+oid sha256:bde9849dd325baf6505b89d85c3a0ee848f2f5997cded42f55047cb0ade63ddb
+size 7164
