69074bab5110b86987f87faa86786221abe8da28
wldnjs35242 <92111247+Jiwon119@users.noreply.github.com>
2024-05-19 18:46:46 +0900

    🎨 Style: 서랍 추가 폼 디자인, 유저 질문 책 표시 개수 설정
    Former-commit-id: f6a2b3d0f5969b3c0a47bcf81cfefde3113c8dae

--- Frontend/src/constants/Type/Type.tsx
+++ Frontend/src/constants/Type/Type.tsx
@@ -1,21 +1,3 @@
-export interface BookType {
-  bookId: number;
-  productId: number;
-  searchKeyword: string;
-  totalClickCount: number;
-  totalOrderCount: number;
-  totalOrderAmount: number;
-  contents: string;
-  productName: string;
-  salePrice: number;
-  categoryName: string;
-  totalPurchaseCount: number;
-  keywords: string[];
-}
-
-export interface PageInfo {
-  page: number;
-  size: number;
-  totalElements: number;
-  totalPages: number;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:551baf8a959baed23dad907c7c79135bc6f2ec47d5664c6d5a9373915519267e
+size 425
