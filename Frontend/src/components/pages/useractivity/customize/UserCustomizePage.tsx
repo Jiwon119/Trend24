15f5eb9fe3647a055c400bd53c9b143af820fc60
김태훈 <kth32105@naver.com>
2024-05-20 09:29:32 +0900

    :fix: Feat: 수정
    Former-commit-id: df39190576f99d33fa4a16cdf33e9140179d0acc

--- Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
+++ Frontend/src/components/pages/useractivity/customize/UserCustomizePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b3d07d649c19e0a9a96dd994f51c41e3036025a66069cf352311f5c6bee9ea0
-size 5655
+oid sha256:d23bc675b800df6cdad4f43e5a90f1e61c3a73234c13eba95b93712a3cf51de9
+size 6015
