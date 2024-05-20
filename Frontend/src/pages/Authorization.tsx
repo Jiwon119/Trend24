7a3e9d5f22fc0b96764466a48bb6baf346014e32
김지원 <wldnjs35242@naver.com>
2024-05-20 10:40:48 +0900

    :fix: Feat: 사이드바 유저 이름, 검색 안내 메세지 수정
    Former-commit-id: 6fdc707753b46a045d4a03e4d81a561596c7b568

--- Frontend/src/pages/Authorization.tsx
+++ Frontend/src/pages/Authorization.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b69d58fb0b9b5f9873cada70e64b3911db01fd6b8342bcad453202c34206e774
-size 7198
+oid sha256:e9caa09fb4fc119e748593ae21e040035d2c01590a9b20d19e88050b0100c68d
+size 7248
